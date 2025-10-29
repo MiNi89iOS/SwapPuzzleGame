6be9b2b79ee7d5be6b883ef61c9bdd7317a40e42
gitmini89-CikiMini <gitmini89@gmail.com>
2025-10-29 08:07:55 +0100

    Initial Commit

---
+++ Swap Puzzle Game/Swap_Puzzle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swap_Puzzle_GameApp.swift
+//  Swap Puzzle Game
+//
+//  Created by Mi Ni on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Swap_Puzzle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
