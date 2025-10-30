60cb97a6fc550c78f182c9b25362750275d7d6e2
gitmini89-CikiMini <gitmini89@gmail.com>
2025-10-30 18:49:00 +0100

    UIImage Normalized update

--- Extensions/UIImage+Normalized.swift
+++ Extensions/UIImage+Normalized.swift
@@ -9,10 +9,15 @@ extension UIImage {
     /// Returns a new image with orientation fixed to .up
     func normalizedImage() -> UIImage {
         guard imageOrientation != .up else { return self }
-        UIGraphicsBeginImageContextWithOptions(size, false, scale)
-        draw(in: CGRect(origin: .zero, size: size))
-        let normalized = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return normalized ?? self
+        
+        let format = UIGraphicsImageRendererFormat.default()
+        format.scale = scale
+        format.opaque = false
+        
+        let renderer = UIGraphicsImageRenderer(size: size, format: format)
+        let normalized = renderer.image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return normalized
     }
 }
